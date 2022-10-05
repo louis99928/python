d0a1c62076f124ad27936da10183ae630d04466f
louis99928 <chichima99928@gmail.com>
2022-10-05 18:49:22 +0800

    提示文字

---
+++ class5/class/class.py
@@ -0,0 +1,21 @@
+# password = input('請輸入密碼')
+# if password == "1234":
+#     print("歡迎光臨")
+# elif password == "8787":
+#     print('歡迎光臨')
+# elif password == "1487":
+#     print('歡迎光臨')
+# else:
+#     print('錯誤')
+
+score = float(input('請輸入成績'))
+if score >= 90:
+    print('your score is A')
+elif score >= 80:
+    print('your score is B')
+elif score >= 70:
+    print('your score is C')
+elif score >= 60:
+    print('your score is D')
+elif score < 60:
+    print('your score is E')
